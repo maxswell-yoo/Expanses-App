eae546bf47acbc3f6789e5b5809196f0329e269b
Maxswell-yoo <max.araujodev@gmail.com>
2023-05-15 14:40:42 -0500

    Usando key global no ListView builder

--- lib/components/transaction_item.dart
+++ lib/components/transaction_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     super.key,
     required this.tr,
@@ -11,6 +13,28 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction tr;
   final void Function(String p1) onRemove;
+
+  @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  static const colors = [
+    Colors.red,
+    Colors.purple,
+    Colors.orange,
+    Colors.blue,
+    Colors.black
+  ];
+  
+  late Color _backgroundColor;
+
+  @override
+  void initState() {
+    super.initState();
+    int i = Random().nextInt(5);
+    _backgroundColor = colors[i];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,31 +46,31 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
+          backgroundColor: _backgroundColor,
           foregroundColor: Colors.white,
           radius: 30,
           child: Padding(
             padding: const EdgeInsets.all(6),
             child: FittedBox(
-              child: Text('R\$${tr.value}'),
+              child: Text('R\$${widget.tr.value}'),
             ),
           ),
         ),
         title: Text(
-          tr.title!,
+          widget.tr.title!,
           style: Theme.of(context).textTheme.titleLarge,
         ),
         subtitle: Text(
-          DateFormat('d MMM y').format(tr.date!)
+          DateFormat('d MMM y').format(widget.tr.date!)
         ),
         trailing: MediaQuery.of(context).size.width > 400 ? 
         TextButton(
-          onPressed: () => onRemove(tr.id!), 
+          onPressed: () => widget.onRemove(widget.tr.id!), 
           child: Text('Remover', style: TextStyle(color: Theme.of(context).colorScheme.error),))
         : IconButton(
           icon: const Icon(Icons.delete),
           color: Theme.of(context).colorScheme.error,
-          onPressed: () => onRemove(tr.id!)),
+          onPressed: () => widget.onRemove(widget.tr.id!)),
       ),
     );
   }
--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,34 +10,35 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? LayoutBuilder(
-        builder: (ctx, constrains) {
-          return Column(
-            children: <Widget>[
-              const SizedBox(height: 20),
-              Text(
-                "Nenhuma Transação Cadastrada",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                height: constrains.maxHeight * 0.6,
-                child: Image.asset(
-                  "assets/images/waiting.png",
-                  fit: BoxFit.cover,
+        ? LayoutBuilder(builder: (ctx, constrains) {
+            return Column(
+              children: <Widget>[
+                const SizedBox(height: 20),
+                Text(
+                  "Nenhuma Transação Cadastrada",
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-              )
-            ],
-          );
-        })
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: constrains.maxHeight * 0.6,
+                  child: Image.asset(
+                    "assets/images/waiting.png",
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            );
+          })
         : ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return TransactionItem(tr: tr, onRemove: onRemove);
+              return TransactionItem(
+                tr: tr,
+                onRemove: onRemove,
+                key: GlobalObjectKey(tr),
+              );
             },
           );
   }
 }
-
-
